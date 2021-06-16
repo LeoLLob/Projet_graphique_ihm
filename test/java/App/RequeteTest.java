f36390f5c99ed26cbd500e7e39db72e24f564608
Mathis Lhoste <82580856+MathisLST@users.noreply.github.com>
2021-06-16 09:35:54 +0200

    test ?

---
+++ test/java/App/RequeteTest.java
@@ -0,0 +1,48 @@
+package App;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RequeteTest {
+
+    @Test
+    void getURL() {
+    }
+
+    @Test
+    void getURLDate() {
+    }
+
+    @Test
+    void getURLZone() {
+    }
+
+    @Test
+    void getURLNom() {
+    }
+
+    @Test
+    void readJsonFromUrl() {
+    }
+
+    @Test
+    void readJsonFromUrlListeNom() {
+    }
+
+    @Test
+    void creerRechercheNom() {
+    }
+
+    @Test
+    void creerRechercheDate() {
+    }
+
+    @Test
+    void creerRechercheZone() {
+    }
+
+    @Test
+    void listeNom() {
+    }
+}
